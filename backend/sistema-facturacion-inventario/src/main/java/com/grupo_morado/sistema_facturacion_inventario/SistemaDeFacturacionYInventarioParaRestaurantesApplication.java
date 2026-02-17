32887a37ce7ffc6037672175bcec3679012e92a7
Julián Andrés Quiroga Lizarazo <103070330+Julian20Quiroga@users.noreply.github.com>
2026-02-17 15:39:09 -0500

    Inicializacion del proyecto
    * inicialización del backend y frontend

---
+++ backend/sistema-facturacion-inventario/src/main/java/com/grupo_morado/sistema_facturacion_inventario/SistemaDeFacturacionYInventarioParaRestaurantesApplication.java
@@ -0,0 +1,13 @@
+package com.grupo_morado.sistema_facturacion_inventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeFacturacionYInventarioParaRestaurantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeFacturacionYInventarioParaRestaurantesApplication.class, args);
+	}
+
+}
---
+++ backend/sistema-facturacion-inventario/src/test/java/com/grupo_morado/sistema_facturacion_inventario/SistemaDeFacturacionYInventarioParaRestaurantesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo_morado.sistema_facturacion_inventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeFacturacionYInventarioParaRestaurantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
