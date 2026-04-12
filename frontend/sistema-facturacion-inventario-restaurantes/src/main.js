e16954fe7426a1aa1ba406a4d32b8efb8f9d1d5a
DaianCastellanos <daiandmcc@gmail.com>
2026-04-11 19:54:31 -0500

    prueba
    Pruebahola2

--- frontend/sistema-facturacion-inventario-restaurantes/src/main.js
+++ frontend/sistema-facturacion-inventario-restaurantes/src/main.js
@@ -3,6 +3,8 @@ import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
+//Hola
+
 document.querySelector('#app').innerHTML = `
   <div>
     <a href="https://vite.dev" target="_blank">
